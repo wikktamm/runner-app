8cacdfe4cb63d56c48a17b7ce4a4a84c04f77166
Wiktoria Tarnowska <wikktamm@gmail.com>
2020-06-29 15:34:22 +0200

    Add model run class and dao object

---
+++ app/src/main/java/com/example/runnerapp/data/room/Run.kt
@@ -0,0 +1,18 @@
+package com.example.runnerapp.data
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "runs")
+data class Run(
+    var image: Bitmap? = null,
+    var timestamp: Long = 0L,
+    var caloriesBurned: Int = 0,
+    var averageSpeedKMH: Float = 0f,
+    var timeInMs: Long = 0L,
+    var distanceInMeters: Int = 0
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
---
+++ app/src/main/java/com/example/runnerapp/data/room/RunDao.kt
@@ -0,0 +1,4 @@
+package com.example.runnerapp.data
+
+interface RunDao {
+}
